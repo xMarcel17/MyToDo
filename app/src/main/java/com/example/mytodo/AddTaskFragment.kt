109fb01716c4d0501ee4969c7aabee6125c0df3b
xMarcel17 <marcelradtke17@gmail.com>
2024-04-04 09:28:57 +0200

    Commit

--- app/src/main/java/com/example/mytodo/AddTaskFragment.kt
+++ app/src/main/java/com/example/mytodo/AddTaskFragment.kt
@@ -5,55 +5,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.mytodo.databinding.FragmentAddTaskBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AddTaskFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddTaskFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding: FragmentAddTaskBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_task, container, false)
+        // Initialize the binding variable
+        binding = FragmentAddTaskBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddTaskFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddTaskFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.saveButton.setOnClickListener { saveTask() }
     }
+
+    private fun saveTask() {
+        // Get the values from data fields on the screen
+        var title: String = binding.titleInput.text.toString()
+        var description: String = binding.descriptionInput.text.toString()
+        val importance = when(binding.importanceGroup.checkedRadioButtonId){
+            R.id.low_radioButton -> IMPORTANCE.LOW
+            R.id.normal_radioButton -> IMPORTANCE.NORMAL
+            R.id.high_radioButton -> IMPORTANCE.HIGH
+            else -> IMPORTANCE.NORMAL
+        }
+        // Handle missing EditText input
+        if(title.isEmpty())
+            title = getString(R.string.default_task_title) + "${Tasks.list.size + 1}"
+        if(description.isEmpty())
+            description = getString(R.string.no_desc_msg)
+        // Create a new Task item based on input values
+        val taskItem = Task(
+            {title + description}.hashCode().toString(),
+            title,
+            description,
+            importance
+        )
+        // Add the new task to the list of tasks
+        Tasks.addTask(taskItem)
+
+        // Hide the software keyboard with InputMethodManager
+        val inputMethodManager: InputMethodManager =
+            activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(binding.root.windowToken,0)
+    }
+
 }
